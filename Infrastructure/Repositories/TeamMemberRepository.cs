a561f6aaf77e0121f65c72169abf6ada3143dbcf
Ilona Kazantseva <Ilona.Kazantseva@urfu.me>
2026-05-16 17:35:05 +0500

    репозитории

---
+++ Infrastructure/Repositories/TeamMemberRepository.cs
@@ -0,0 +1,17 @@
+using Infrastructure.Db;
+using Infrastructure.Enums;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class TeamMemberRepository(DatabaseContext context) : ITeamMemberRepository
+{
+    public async Task<bool> IsAdminAsync(Guid teamId, Guid userId, CancellationToken cancellationToken)
+    {
+        return await context.TeamMembers
+            .Where(x => x.TeamId == teamId && x.UserId == userId)
+            .Select(x => x.Role == TeamRole.Admin)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+}
